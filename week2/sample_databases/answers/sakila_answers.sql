0b77a3de8c3a9c052d8960443a918e09cd1915de
Nasim <nassim.karimi.nk@gmail.com>
2024-10-22 15:45:44 +0100

    EDIT: answers to the second 3 questions

--- week2/sample_databases/answers/sakila_answers.sql
+++ week2/sample_databases/answers/sakila_answers.sql
@@ -9,3 +9,17 @@ WHERE first_name = "John";
 SELECT first_name FROM actor 
 WHERE last_name = "Neeson";
 
+#Q4
+SELECT actor_id, first_name, last_name
+FROM actor
+WHERE mod(actor_id, 10) = 0;
+
+#Q5
+SELECT description
+FROM film
+WHERE film_id = 100;
+
+#Q6
+SELECT film_id, title, rating 
+FROM film 
+WHERE rating = "R";
